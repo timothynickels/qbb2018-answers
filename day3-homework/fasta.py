1c76ede9e2bbf73dadb6335a004b76caec307a91
Timothy Nickels <tnickel2@jhu.edu>
2018-08-29 20:20:40 -0400

    added fasta.py file

---
+++ day3-homework/fasta.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#parse all FASTA records from stdin and print id and sequence
+
+import sys
+
+class FASTAReader(object):
+    
+    def __init__(self,file):
+        self.last_ident = None
+        self.file = file
+        self.eof = False
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.eof:
+            raise StopIteration
+        if self.last_ident is not None:
+            #Not first line
+            ident = self.last_ident
+            
+        else:
+            #First line
+            line = self.file.readline()
+            if line == "":
+                return None, None
+            
+            assert line.startswith(">"), "Not a FASTA file"
+            ident = line[1:].rstrip("\r\n") 
+
+        sequences = []
+        while True:
+            line = self.file.readline()
+            if line == "":
+                self.eof = True
+                break
+            elif not line.startswith(">"):
+                sequences.append(line.strip())
+            else:
+                self.last_ident = line[1:].rstrip("\r\n")
+                break
+        sequence = "".join(sequences)
+        return ident, sequence
